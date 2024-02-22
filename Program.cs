5db4ea4107b2298e2a6b067027da216aedecbc26
Vlad <vladex.tv@mail.ru>
2024-02-22 12:05:24 +0300

    Добавьте файлы проекта.

---
+++ Program.cs
@@ -0,0 +1,10 @@
+﻿// See https://aka.ms/new-console-template for more information
+class Program
+{
+    static void Main()
+    { 
+        int x =Convert.ToInt32(Console.ReadLine());
+        int y = Convert.ToInt32(Console.ReadLine());
+        Console.WriteLine(x-Math.Pow(Math.Sin(x),10)+Math.Cos(x-y));
+    }
+}
